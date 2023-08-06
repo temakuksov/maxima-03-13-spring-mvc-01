d0def678e758e3b30e4f28cfa56906d9a771bda0
Kuksov Artem <35718107+temakuksov@users.noreply.github.com>
2023-08-07 03:25:58 +0500

    Spring MVC 01

--- src/main/java/ru/maxima/springmvc/controllers/FirstController.java
+++ src/main/java/ru/maxima/springmvc/controllers/FirstController.java
@@ -1,7 +1,9 @@
 package ru.maxima.springmvc.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class FirstController {
     @GetMapping("/hello")
     public String hello() {
